f6699da1c731b58e6af80837426778005bbd1561
alx4n <nbratek99@gmail.com>
2026-01-27 20:27:56 -0500

    Finish TS Intro Activity

--- sample_express/index.js
+++ sample_express/index.js
@@ -1,11 +1,37 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { routeHello, routeAPINames, routeWeather } from './routes.js';
 import express from 'express';
-import { helloCallback, namesRoute } from './routes.js';
 const server = express();
 const port = 3000;
-
-server.get('/hello', helloCallback);
-server.get('/api/calls', namesRoute);
-
-server.listen(port, function() {
+server.get('/hello', function (_req, res) {
+    const response = routeHello();
+    res.send(response);
+});
+server.get('/api/names', function (_req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let response;
+        try {
+            response = yield routeAPINames();
+            res.send(response);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+});
+server.get("/api/weather/:zipcode", function (req, res) {
+    const response = routeWeather({ zipcode: JSON.stringify(req.params.zipcode) });
+    res.send(response);
+});
+server.listen(port, function () {
     console.log('Listening on ' + port);
 });
+//# sourceMappingURL=index.js.map
